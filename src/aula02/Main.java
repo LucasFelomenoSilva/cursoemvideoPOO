c8a606ae74ac0978e9175b3ceac4e079baee945f
Lucas <pv.lucassilva@gmail.com>
2024-06-18 12:30:54 -0300

    add atributes and methods

--- src/aula02/Main.java
+++ src/aula02/Main.java
@@ -2,7 +2,18 @@ package aula02;
 
 public class Main {
     public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.tampar();
+        c1.status();
+        c1.rabiscar();
 
-
+        Caneta c2 = new Caneta();
+        c2.modelo = "Bic";
+        c2.cor = "Preta";
+        c2.destampar();
+        c2.status();
+        c2.rabiscar();
     }
 }
