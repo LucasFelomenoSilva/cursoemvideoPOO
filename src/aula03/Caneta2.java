cf4c32fcffa337a7053b9cbbd9ca4820146d9fc1
Lucas <pv.lucassilva@gmail.com>
2024-06-19 08:50:14 -0300

    add and repare aula03

---
+++ src/aula03/Caneta2.java
@@ -0,0 +1,30 @@
+package aula03;
+
+public class Caneta2 {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    void status() { //retorno
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
+    }
+
+    void rabiscar(){
+        if (this.tampada == true) {
+            System.out.println("Erro! Não posso rabiscar.");
+        } else {
+            System.out.println("Rabiscando.");
+        }
+    }
+    void tampar(){
+        this.tampada = true;
+    }
+    void destampar(){
+        this.tampada = false;
+    }
+}
---
+++ src/aula03/Main2.java
@@ -0,0 +1,10 @@
+package aula03;
+
+import aula02.Caneta;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.status();
+    }
+}
