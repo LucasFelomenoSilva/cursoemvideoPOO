3e52a75d986f0d013c6c542a0cdb283aa350d66d
Lucas <pv.lucassilva@gmail.com>
2024-06-20 09:09:30 -0300

    add aula04 'getters, setters and constructor'

---
+++ src/aula04/Caneta3.java
@@ -0,0 +1,54 @@
+package aula04;
+
+public class Caneta3 {
+    public String modelo;
+    private float ponta;
+    private boolean tampada;
+    private String cor;
+
+    public Caneta3(String modelo, String cor, float ponta) {
+        this.modelo = modelo;
+        this.ponta = ponta;
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public float getPonta() {
+        return ponta;
+    }
+
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+
+    public boolean getTampada() {
+        return tampada;
+    }
+
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public void status() {
+        System.out.println("DADOS DA CANETA");
+        System.out.println("Modelo: " + getModelo());
+        System.out.println("Ponta: " + getPonta());
+        System.out.println("Cor: " + getCor());
+        System.out.println("Tampada: " + getTampada());
+    }
+}
---
+++ src/aula04/Program.java
@@ -0,0 +1,8 @@
+package aula04;
+
+public class Program {
+    public static void main(String[] args) {
+        Caneta3 c1 = new Caneta3("NIC", "Amarelo", 0.4f);
+        c1.status();
+    }
+}
