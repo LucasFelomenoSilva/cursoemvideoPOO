74eb7942ac45110d531e59c926372531b3f52486
Lucas <pv.lucassilva@gmail.com>
2024-06-20 09:51:19 -0300

    add aula05

---
+++ src/aula05/Banco.java
@@ -0,0 +1,21 @@
+package aula05;
+
+public class Banco {
+    public String numConta;
+    protected String tipo;
+    private String dono;
+    private float saldo;
+    private String status;
+
+    public void abrirConta() {
+    }
+    public void fecharConta() {
+    }
+    public void depositar() {
+    }
+    public void sacar() {
+    }
+    public void pagarMensal() {
+    }
+
+}
