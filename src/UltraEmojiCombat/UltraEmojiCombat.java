a0a6ce3dba384baed5de0cdd687dcea6046e27b2
Lucas Silva <pv.lucassilva@gmail.com>
2024-06-21 18:19:20 -0300

    add Luta.java and upd

--- src/UltraEmojiCombat/UltraEmojiCombat.java
+++ src/UltraEmojiCombat/UltraEmojiCombat.java
@@ -10,6 +10,7 @@ public class UltraEmojiCombat {
         l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
         l[5] = new Lutador("Nerdaart", "EUA", 62, 1.81f, 105.7f, 12, 2, 4);
 
-        l[3].status();
+
+
     }
 }
