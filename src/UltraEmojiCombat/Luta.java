2e867c5044779d68bb5e0719f54cf9e0eabef091
Lucas Silva <pv.lucassilva@gmail.com>
2024-06-21 18:31:16 -0300

    att luta and ultraemojicombat

--- src/UltraEmojiCombat/Luta.java
+++ src/UltraEmojiCombat/Luta.java
@@ -9,7 +9,7 @@ public class Luta {
     private boolean aprovada;
 
     public void marcarLuta(Lutador l1, Lutador l2) {
-        if (l1.getCategoria().equals(l2.getCategoria()) && l1 != l2 ) {
+        if (l1.getCategoria().equals(l2.getCategoria()) && l1 != l2) {
             this.aprovada = true;
             this.desafiado = l1;
             this.desafiante = l2;
@@ -24,7 +24,7 @@ public class Luta {
             System.out.println("### DESAFIADO ###");
             this.desafiado.apresentar();
             System.out.println("### DESAFIANTE ###");
-            this.desafiado.apresentar();
+            this.desafiante.apresentar();
 
             Random aleatorio = new Random();
             int vencedor = aleatorio.nextInt(3);
@@ -38,11 +38,15 @@ public class Luta {
                     System.out.println("Vitória do " + this.desafiado.getNome());
                     this.desafiado.ganharLuta();
                     this.desafiante.perderLuta();
+                    this.desafiado.status();
+                    this.desafiante.status();
                     break;
                 case 2:
                     System.out.println("Vitória do " + this.desafiante.getNome());
                     this.desafiante.ganharLuta();
                     this.desafiado.perderLuta();
+                    this.desafiante.status();
+                    this.desafiado.status();
                     break;
             }
 
