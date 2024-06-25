60053d85bb4161db74f7cbf6ae62b44b9e19d9db
Lucas <pv.lucassilva@gmail.com>
2024-06-25 12:20:13 -0300

    add 'aula09'

---
+++ src/aula09/Aluno.java
@@ -0,0 +1,27 @@
+package aula09;
+
+public class Aluno extends Pessoa{
+    private String matr;
+    private String curso;
+
+    public void cancelarMatr() {
+
+    }
+
+
+    public String getMatr() {
+        return matr;
+    }
+
+    public void setMatr(String matr) {
+        this.matr = matr;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+}
