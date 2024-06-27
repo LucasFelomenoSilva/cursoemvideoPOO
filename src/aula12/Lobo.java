65ff224f58676d71f666832860a249114c0cb29a
Lucas <pv.lucassilva@gmail.com>
2024-06-27 08:05:55 -0300

    add aula12

---
+++ src/aula12/Lobo.java
@@ -0,0 +1,10 @@
+package aula12;
+
+import aula12.Mamifero;
+
+public class Lobo extends Mamifero {
+    @Override
+    public void emitirSom() {
+        System.out.println("Auuuuuuuuuuuuuuuuu");
+    }
+}
