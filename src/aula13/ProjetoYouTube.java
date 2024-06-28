5f14b38b7e2ea1d6e1afcfa9562b0061b6af5dab
Lucas <pv.lucassilva@gmail.com>
2024-06-28 08:20:41 -0300

    add visualizacao.java

--- src/aula13/ProjetoYouTube.java
+++ src/aula13/ProjetoYouTube.java
@@ -11,8 +11,25 @@ public class ProjetoYouTube {
         u[0] = new Usuario("Lucas", 21, "M", "lucassilva");
         u[1] = new Usuario("Carla", 24, "F", "carlateste");
 
+        Visualizacao vis[] = new Visualizacao[5];
+
+        vis[0] = new Visualizacao(u[0], v[2]);
+        vis[0].avaliar();
+        System.out.println(vis[0].toString());
+
+        vis[1] = new Visualizacao(u[0], v[1]);
+        vis[0].avaliar(87.0f);
+        System.out.println(vis[1].toString());
+
+        /*System.out.println("VÍDEOS\n-----------------------");
         System.out.println(v[0].toString());
+        System.out.println(v[1].toString());
+        System.out.println(v[2].toString());
+        System.out.println("USUÁRIOS\n-----------------------");
         System.out.println(u[0].toString());
+        System.out.println(u[1].toString());*/
+
+
     }
 
 }
